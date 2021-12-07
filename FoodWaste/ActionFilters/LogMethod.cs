3f66f7a7b2b7b2d9731c229c4f2ced34e6d114d8
Vaidas Čibiras <55850111+siturez@users.noreply.github.com>
2021-12-07 22:52:48 +0200

    Implemented logging with IActionFilter

---
+++ FoodWaste/ActionFilters/LogMethod.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodWaste.ActionFilters
+{
+    public class LogMethod : IActionFilter
+    {
+        private readonly ILogger<LogMethod> _logger;
+        public LogMethod(ILogger<LogMethod> logger)
+        {
+            _logger = logger;
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            _logger.LogInformation("Completed: Page opened: {0}", context.ActionDescriptor.DisplayName);
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            _logger.LogInformation("Start: Opening page: {0}", context.ActionDescriptor.DisplayName);
+        }
+    }
+}
